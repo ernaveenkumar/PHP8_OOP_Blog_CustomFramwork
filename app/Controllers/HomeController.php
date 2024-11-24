4b9d95836ce6ee9ce6573649ce5ced67e08908a8
Naveen Kumar <er.crackerjack@gmail.com>
2024-11-24 14:04:04 +1300

    Till this point Working PHP Framework is ready to use and we can by using this framework can implement our required functionality of application

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -7,19 +7,19 @@ use Core\View;
 class HomeController{
   public function index(){
 
-    User::create([
-      'name' => 'John doe',
-      'email' => 'er.johndoe@gmail.com',
-      'role' => 'admin',
-      'password' => password_hash('admin', PASSWORD_DEFAULT)
-    ]);
-
     // User::create([
-    //   'name' => "Naveen Kumar",
-    //   'email' => 'er.crackerjack@gmail.com',
-    //   'role' => 'admn',
-    //   'password' => password_hash('naveen@6', PASSWORD_DEFAULT)
+    //   'name' => 'John doe',
+    //   'email' => 'er.johndoe@gmail.com',
+    //   'role' => 'admin',
+    //   'password' => password_hash('admin', PASSWORD_DEFAULT)
     // ]);
+
+    User::create([
+      'name' => "Naveen Kumar",
+      'email' => 'er.crackerjack@gmail.com',
+      'role' => 'admn',
+      'password' => password_hash('naveen@6', PASSWORD_DEFAULT)
+    ]);
 
     return View::render(
       template:'home/index',
